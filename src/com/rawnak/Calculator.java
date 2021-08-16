22c012bdf8ab2a6eabd9590b405733d13cd2954a
rawnak-yazdani <rawnakasif@gmail.com>
2021-08-16 10:54:43 +0600

    Initial commit

---
+++ src/com/rawnak/Calculator.java
@@ -0,0 +1,19 @@
+package com.rawnak;
+
+public class Calculator {
+
+    int performAddition(int a, int b){
+        int result = a + b;
+        return result;
+    }
+
+    int performAddition(int a, int b, int c, int d){
+        int result = a + b + c + d;
+        return result;
+    }
+
+    float performAddition(float a, float b){
+        float result = a + b;
+        return result;
+    }
+}
