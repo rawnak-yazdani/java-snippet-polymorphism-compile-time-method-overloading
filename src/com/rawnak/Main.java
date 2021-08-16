3792b711a908c13b8f92d6b92d99f4e3c68db190
rawnak-yazdani <rawnakasif@gmail.com>
2021-08-16 11:18:28 +0600

    update

--- src/com/rawnak/Main.java
+++ src/com/rawnak/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Calculator calculator = new Calculator();
+
+        int intAdditionOfTwoNumbers = calculator.performAddition(2,3);
+        int intAdditionOfFourNumbers = calculator.performAddition(2, 3, 7, 10);
+        float floatAddition = calculator.performAddition(2.5f, 3.7f);
+
+        System.out.println(intAdditionOfTwoNumbers);
+        System.out.println(intAdditionOfFourNumbers);
+        System.out.println(floatAddition);
     }
 }
